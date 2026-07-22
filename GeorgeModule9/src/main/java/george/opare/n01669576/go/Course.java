180768d183f895b17865257760e3300d57a37b4a
GeorgeOpare9576 <georgeopare1201@icloud.com>
2026-07-22 13:44:37 -0400

    Add Fragment 1 layout with course fields, buttons and RecyclerView

---
+++ GeorgeModule9/src/main/java/george/opare/n01669576/go/Course.java
@@ -0,0 +1,21 @@
+// George Opare n01669576
+package george.opare.n01669576.go;
+
+public class Course {
+
+    private String courseName;
+    private String courseDescription;
+
+    public Course(String courseName, String courseDescription) {
+        this.courseName = courseName;
+        this.courseDescription = courseDescription;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public String getCourseDescription() {
+        return courseDescription;
+    }
+}
